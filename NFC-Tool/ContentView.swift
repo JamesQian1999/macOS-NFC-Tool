198e87e2131acc6da36986a11b8324172d731578
Chien Cheng <jamesqian1999@gmail.com>
2023-12-30 22:20:21 +0800

    first design

--- NFC-Tool/ContentView.swift
+++ NFC-Tool/ContentView.swift
@@ -22,6 +22,11 @@ func shell(_ command: String) -> String {
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     let output = String(data: data, encoding: .utf8)!
     
+    print("#################")
+    print("# Shell message #")
+    print("#################")
+    print(output)
+    
     return output
 }
 
@@ -36,18 +41,24 @@ struct ContentView: View {
             // UID display
             Text("UID: \(uid)")
                 .padding()
+                .font(.system(size: 30))
+                .monospaced()
 
             // Buttons for UID actions
             Button(action: {
                 readUID()
             }) {
-                Text("Read UID")
+                Text("Read  UID")
+                    .font(.system(size: 20))
+                    .monospaced()
             }
 
             Button(action: {
                 writeUID()
             }) {
                 Text("Write UID")
+                    .font(.system(size: 20))
+                    .monospaced()
             }
 
             // Log display area with auto-scroll
@@ -56,6 +67,7 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         ForEach(logMessages.indices, id: \.self) { index in
                             Text(logMessages[index])
+                                .font(.system(.body, design: .monospaced))
                                 .id(index) // Assign ID for each log message
                         }
                     }
@@ -69,45 +81,84 @@ struct ContentView: View {
                     }
                 }
             }
+            
+            Spacer() // Pushes everything up
+
+            HStack {
+                Spacer() // Pushes text to the right
+                VStack(alignment: .leading) {
+                    Text("Designed by Cheng Chien")
+                    Text("cchen1999@cs.nycu.edu.tw")
+                }
+                .padding()
+            }
 
         }
         .padding()
         .onAppear {
-                    performInitialSetup()
-                }
+                performInitialSetup()
+            }
     }
     
+    private func addlog(messages : String) -> Void {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let yourDate = formatter.date(from: formatter.string(from: Date()))
+        let myStringDate = formatter.string(from: yourDate!)
+        var log = "[" + myStringDate + "]" + "   " + messages
+        logMessages.append(log)
+        print(log)
+    }
 
     private func performInitialSetup() {
         // Your initial setup code here
         // For example, you could read the initial UID or log a welcome message
-        let device = shell("/opt/homebrew/bin/nfc-list").components(separatedBy: "\n")
-        logMessages.append(device[1])
-        print(device)
+        let device = shell("/opt/homebrew/bin/nfc-list")
+        let findDevice = /NFC\ device:.*\ /
+        
+        if let DEVICE = try! findDevice.firstMatch(in: device){
+            addlog(messages: String(DEVICE.0))
+        }
+        
     }
 
     // Function to simulate reading UID
     private func readUID() {
-        let readMessage = shell("/opt/homebrew/bin/nfc-list").components(separatedBy: "\n")
+        let readMessage = shell("/opt/homebrew/bin/nfc-list")
+        
+        let findUID = /UID.*: [0-9a-zA-Z][0-9a-zA-Z]  [0-9a-zA-Z][0-9a-zA-Z]  [0-9a-zA-Z][0-9a-zA-Z]  [0-9a-zA-Z][0-9a-zA-Z]/
+        
         var log = ""
-        if readMessage.count <= 3 {
-            uid = "Unknow UID or Empty"
-            log = "Unknow UID or Empty"
+        if let UID = try! findUID.firstMatch(in: readMessage) {
+            uid = String(UID.0).components(separatedBy: ":")[1].replacingOccurrences(of: "  ", with: " ")
+            log = "Read UID: " + uid
         }
         else {
-            uid = readMessage[5].components(separatedBy: ":")[1]
-            log = "Read " + readMessage[5]
+            uid = "Unknow UID or No card"
+            log = "Read error. Unknow UID or No card"
         }
-                
-        logMessages.append(log)
+        
+        addlog(messages:log)
     }
 
     // Function to simulate writing UID
     private func writeUID() {
-        let writeMessage = "Write UID: ABCD-EF12-3456-7890"
-        uid = "ABCD-EF12-3456-7890"
-        logMessages.append(writeMessage)
-        print(writeMessage)
+        let CheckedUID = uid.replacingOccurrences(of: " ", with: "")
+        print(CheckedUID)
+        let findUID = /^[0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z]$/
+        
+        var log = ""
+        if (try! findUID.firstMatch(in: CheckedUID)) != nil {
+            let ShellMessage = shell("/opt/homebrew/bin/nfc-mfsetuid -f" + CheckedUID)
+            log = "Write UID:" + uid
+        }
+        else {
+            uid = "Unknow UID or No card"
+            log = "Write error. Unknow UID or No card"
+        }
+        
+        
+        addlog(messages:log)
     }
 }
 
