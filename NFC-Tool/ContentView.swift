b85c5b3dd0525d6b9d88af8d5256e11e41c856eb
Chien Cheng <jamesqian1999@gmail.com>
2023-12-31 00:15:53 +0800

    v1.1 install brew and libnfc

--- NFC-Tool/ContentView.swift
+++ NFC-Tool/ContentView.swift
@@ -2,124 +2,173 @@
 //  ContentView.swift
 //  NFC-Tool
 //
-//  Created by 錢承 on 2023/12/30.
+//  Created by Cheng Chien on 2023/12/30.
 //
 
 import SwiftUI
 import Foundation
 
-func shell(_ command: String) -> String {
-    let task = Process()
-    let pipe = Pipe()
-    
-    task.standardOutput = pipe
-    task.standardError = pipe
-    task.arguments = ["-c", command]
-    task.launchPath = "/bin/zsh"
-    task.standardInput = nil
-    task.launch()
-    
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8)!
-    
-    print("#################")
-    print("# Shell message #")
-    print("#################")
-    print(output)
-    
-    return output
+func checkIfFileOrDirectoryExists(path: String, name: String) -> Bool {
+    let fileManager = FileManager.default
+    let fullPath = (path as NSString).appendingPathComponent(name)
+    return fileManager.fileExists(atPath: fullPath)
 }
 
-
 struct ContentView: View {
-    @State private var uid: String = "UID will appear here"
-    @State private var logMessages: [String] = []
+    @State private var uid: String = "UID will Appear Here"
+    @State private var currentLogSet = 0
+       @State private var logMessages: [[String]] = [[],[]]
 
     var body: some View {
     
-        VStack {
-            // UID display
-            Text("UID: \(uid)")
-                .padding()
-                .font(.system(size: 30))
-                .monospaced()
-
-            // Buttons for UID actions
-            Button(action: {
-                readUID()
-            }) {
-                Text("Read  UID")
-                    .font(.system(size: 20))
+        GeometryReader { geometry in
+            VStack {
+                // UID display
+                Text("UID: \(uid)")
+                    .padding()
+                    .font(.system(size: 30))
                     .monospaced()
-            }
+            
 
-            Button(action: {
-                writeUID()
-            }) {
-                Text("Write UID")
-                    .font(.system(size: 20))
-                    .monospaced()
-            }
+                // Buttons for UID actions
+                Button(action: {
+                    readUID()
+                }) {
+                    Text("Read  UID")
+                        .font(.system(size: 20))
+                        .monospaced()
+                }
 
-            // Log display area with auto-scroll
-            ScrollViewReader { scrollView in
-                ScrollView {
-                    VStack(alignment: .leading) {
-                        ForEach(logMessages.indices, id: \.self) { index in
-                            Text(logMessages[index])
-                                .font(.system(.body, design: .monospaced))
-                                .id(index) // Assign ID for each log message
+                Button(action: {
+                    writeUID()
+                }) {
+                    Text("Write UID")
+                        .font(.system(size: 20))
+                        .monospaced()
+                }
+                
+                Button("Show full logs") {
+                    currentLogSet = (currentLogSet + 1) % logMessages.count
+                }
+
+                // Log display area with auto-scroll
+                ScrollViewReader { scrollView in
+                    ScrollView {
+                        VStack(alignment: .leading) {
+                            ForEach(logMessages[currentLogSet].indices, id: \.self) { index in
+                                Text(logMessages[currentLogSet][index])
+                                    .font(.system(.body, design: .monospaced))
+                                    .id(index) // Assign ID for each log message
+                            }
+                        }
+                        .padding(5)
+                    }
+                    .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.5)
+                    .border(Color.gray, width: 2)
+                    .onChange(of: logMessages[currentLogSet]) { _ in
+                        withAnimation {
+                            scrollView.scrollTo(logMessages[currentLogSet].count - 1, anchor: .bottom)
                         }
                     }
-                    .padding(5)
                 }
-                .frame(width: 800, height: 200)
-                .border(Color.gray, width: 2)
-                .onChange(of: logMessages) { _ in
-                    withAnimation {
-                        scrollView.scrollTo(logMessages.count - 1, anchor: .bottom)
+                
+                Spacer() // Pushes everything up
+
+                HStack {
+                    Spacer() // Pushes text to the right
+                    VStack(alignment: .leading) {
+                        Text("Designed by Cheng Chien")
+                        Text("cchen1999@cs.nycu.edu.tw")
                     }
+                    .padding()
                 }
-            }
-            
-            Spacer() // Pushes everything up
 
-            HStack {
-                Spacer() // Pushes text to the right
-                VStack(alignment: .leading) {
-                    Text("Designed by Cheng Chien")
-                    Text("cchen1999@cs.nycu.edu.tw")
-                }
-                .padding()
             }
-
-        }
-        .padding()
-        .onAppear {
+            .padding()
+            .onAppear {
                 performInitialSetup()
             }
+        }
+    }
+    private func shell(_ command: String) -> String {
+        let task = Process()
+        let pipe = Pipe()
+        
+        task.standardOutput = pipe
+        task.standardError = pipe
+        task.arguments = ["-c", command]
+        task.launchPath = "/bin/bash"
+        task.standardInput = nil
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+        
+        print("Command: ",command)
+        addlog(messages: "Command: "+command, FullOnly: true)
+        print("#################")
+        print("# Shell message #")
+        print("#################")
+        print(output)
+        addlog(messages: output, FullOnly: true, ShowTime: false)
+        
+        
+        return output
     }
     
-    private func addlog(messages : String) -> Void {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        let yourDate = formatter.date(from: formatter.string(from: Date()))
-        let myStringDate = formatter.string(from: yourDate!)
-        var log = "[" + myStringDate + "]" + "   " + messages
-        logMessages.append(log)
+    private func addlog(messages : String, FullOnly: Bool = false, ShowTime: Bool = true) -> Void {
+        var log = messages
+        
+        if ShowTime {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            let yourDate = formatter.date(from: formatter.string(from: Date()))
+            let myStringDate = formatter.string(from: yourDate!)
+            log = "[" + myStringDate + "]" + "   " + messages
+        }
+        
+        if FullOnly {
+            logMessages[1].append(log)
+        }
+        else {
+            logMessages[0].append(log)
+            logMessages[1].append(log)
+        }
         print(log)
     }
 
     private func performInitialSetup() {
-        // Your initial setup code here
-        // For example, you could read the initial UID or log a welcome message
+        var ShellMsg = "", command = ""
+        if checkIfFileOrDirectoryExists(path: "/opt", name: "homebrew") {
+            if checkIfFileOrDirectoryExists(path: "/opt/homebrew/bin", name: "nfc-list") {
+                
+            } else {
+                print("nfc-list does not exist, installing...")
+                addlog(messages: "nfc-list does not exist, installing...")
+                
+                command = "/opt/homebrew/bin/brew install libnfc"
+                ShellMsg = shell(command)
+                addlog(messages: ShellMsg)
+            }
+        } 
+        else {
+            print("homebrew and nfc-list does not exist, installing...")
+            addlog(messages: "homebrew and nfc-list does not exist, installing...")
+            
+            command = "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+            ShellMsg = shell(command)
+            addlog(messages: ShellMsg)
+        
+            command = "/opt/homebrew/bin/brew install libnfc"
+            ShellMsg = shell(command)
+            addlog(messages: ShellMsg)
+        }
+        
         let device = shell("/opt/homebrew/bin/nfc-list")
         let findDevice = /NFC\ device:.*\ /
-        
         if let DEVICE = try! findDevice.firstMatch(in: device){
             addlog(messages: String(DEVICE.0))
         }
-        
     }
 
     // Function to simulate reading UID
@@ -149,20 +198,18 @@ struct ContentView: View {
         
         var log = ""
         if (try! findUID.firstMatch(in: CheckedUID)) != nil {
-            let ShellMessage = shell("/opt/homebrew/bin/nfc-mfsetuid -f" + CheckedUID)
+            _ = shell("/opt/homebrew/bin/nfc-mfsetuid -f " + CheckedUID)
             log = "Write UID:" + uid
         }
         else {
             uid = "Unknow UID or No card"
             log = "Write error. Unknow UID or No card"
         }
-        
-        
         addlog(messages:log)
     }
 }
 
-// Preview
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
